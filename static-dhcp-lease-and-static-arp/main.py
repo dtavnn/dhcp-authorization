036d82947285845d7f7aa1816ba2c7a20201d61f
xdnroot <admin@tokomini.net>
2021-02-20 22:56:02 +0700

    'todo: make static-leases'

--- static-dhcp-lease-and-static-arp/main.py
+++ static-dhcp-lease-and-static-arp/main.py
@@ -15,11 +15,14 @@ leases = api.get_resource('ip/dhcp-server/lease')
 dhcp = leases.get(mac_address=mac)
 
 # fetch all data
-print(json.dumps(dhcp, indent=4))
+#print(json.dumps(dhcp, indent=4))
 
+# TODO: make static lease
 for item in dhcp:
-    # print the id only
-    print(item['id'])
+    # get the id only
+    id = item['id']
+    print("id =" + id)
+    leases.set(id=id, address="10.10.10.20")
 
 # close the connection
 connection.disconnect()
