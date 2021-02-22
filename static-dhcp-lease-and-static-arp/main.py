3a5b752971fc6055580fba119b2d376d8553667b
xdnroot <admin@tokomini.net>
2021-02-22 21:51:52 +0700

    'update webhook'

--- static-dhcp-lease-and-static-arp/main.py
+++ static-dhcp-lease-and-static-arp/main.py
@@ -175,7 +175,7 @@ def webhook():
         input = request.get_json()
         print(json.dumps(input, indent=4))
         message_id = input['callback_query']['message']['message_id']
-        message_data = input['callback_query']['message']['data']
+        message_data = input['callback_query']['data']
         input = json.load(message_data)
 
         netmiko = netmiko_conn(router, username, password)
