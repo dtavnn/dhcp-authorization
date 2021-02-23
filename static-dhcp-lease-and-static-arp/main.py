f25a50ecc30482bfd8d6ca32862502ced02c2982
xdnroot <admin@tokomini.net>
2021-02-23 11:12:32 +0700

    'fix logging'

--- static-dhcp-lease-and-static-arp/main.py
+++ static-dhcp-lease-and-static-arp/main.py
@@ -75,17 +75,11 @@ def msgencode(text):
     text = text.replace('}', '\\}')
     text = text.replace('.', '\\.')
     text = text.replace('!', '\\!')
-    print(text)
     return text
 
-# loggin [
-#   "mac": {
-#       "comment": "XXX"
-#       "ip": "X.X.X.X"
-#   }
-# ]   
+## add data to logs
 def logging(data):
-    print('logging data')
+    print('******add data to logs******')
     with open(os.environ.get('LOG_FILE'), "r+") as file: 
         object = json.load(file) 
         for key in data:
@@ -98,65 +92,39 @@ def logging(data):
         file.seek(0)
         json.dump(object, file, indent = 4)
         file.truncate()
-    
-    print(json.dumps(data, indent = 4))
 
-
-
-## begin /static_arp
-@app.route('/static_arp', methods = ['POST'])
-def static_arp():
-
-    netmiko = netmiko_conn(router, username, password)
-    rosapi = rosapi_conn(router, username, password)
-    api = rosapi.get_api()
-
-    # get dhcp lease based on mac-address
-    leases = api.get_resource('ip/dhcp-server/lease')
-    dhcp = leases.get(dynamic="yes")
-    return jsonify(dhcp)
-
-    if dhcp:
-        result = {
-            'status': True,
-            'data': []
-        }
-        print(result)
-        x = 0
-        for item in dhcp:
-            # get the id only
-            mac_address = item['mac-address']
-            result['data'].append(mac_address)
-            netmiko.send_config_set('/ip dhcp-server lease make-static [find mac-address=' + mac_address + ']')
-            x+=1
-        
-        logout(netmiko, rosapi)
-        return jsonify(result)
-
-    else:
-        result = {
-            'status': False,
-            'data': None
-        }
-        logout(netmiko, rosapi)
-        return jsonify(result)
-
-    # close the connection
-
-def sendMessage(message):
+## send message to telegram
+def sendMessage(message, keyboard=None):
     url = "https://api.telegram.org/bot" + api_bot + "/sendMessage"
     headers = {
         'Content-Type': 'application/json'
-        }
-    payload = {
-        "chat_id": chat_id,
-        "text": message,
-        "parse_mode": "MarkdownV2",
-        "disable_web_page_preview": True
     }
+
+    if keyboard:
+        headers = {
+        'Content-Type': 'application/json'
+        }
+        payload = {
+            "chat_id": chat_id,
+            "text": message,
+            "parse_mode": "MarkdownV2",
+            "disable_web_page_preview": True,
+            "reply_markup": keyboard
+        }
+        response = requests.request("POST", url, headers=headers, data=json.dumps(payload)).json()
+        print(response)
+    else:
+        payload = {
+            "chat_id": chat_id,
+            "text": message,
+            "parse_mode": "MarkdownV2",
+            "disable_web_page_preview": True
+        }
+
     response = requests.request("POST", url, headers=headers, data=json.dumps(payload)).json()
     print(response)
 
+## delete message by message_id
 def deleteMessage(message_id):
     url = "https://api.telegram.org/bot" + api_bot + "/deleteMessage"
     headers = {
@@ -169,67 +137,75 @@ def deleteMessage(message_id):
     response = requests.request("POST", url, headers=headers, data=json.dumps(payload)).json()
     print(response)
 
+
+## authorize new connected device
+def authorization(message_id, message_data):
+    input = json.loads(message_data)
+
+    netmiko = netmiko_conn(router, username, password)
+    rosapi = rosapi_conn(router, username, password)
+    api = rosapi.get_api()
+
+    # get dhcp lease based on mac-address
+    leases = api.get_resource('ip/dhcp-server/lease')
+    dhcp = leases.get(mac_address=input['mac'], dynamic="yes")
+
+    if dhcp:
+        result = {}
+        for item in dhcp:
+            # get the id only
+            host = item['host-name']
+            ip = item['address']
+            mac = item['mac-address']
+            interface = os.environ.get('DHCP_INTERFACE')
+
+            if input['action'] == 'allow':
+                comment = "allow " + host
+                netmiko.send_config_set([
+                    '/ip dhcp-server lease make-static [find mac-address=' + mac + ']',
+                    '/ip dhcp-server lease set [find mac-address=' + mac + '] comment=' + comment,
+                    '/ip arp add address=' + ip + ' mac-address=' + mac + ' interface=' + interface
+                ])
+                sendMessage("✅ Device Allowed ✅\nHostname: *" + msgencode(host) +
+                    "*\nIP: *" + msgencode(ip) + "*\nMAC Address: *" + mac + "*"
+                )
+                deleteMessage(message_id)
+            else:
+                comment = "deny " + host
+                netmiko.send_config_set([
+                    '/interface wireless access-list add authentication=no forwarding=no mac-address=' + mac + ' comment="' + comment + '"',
+                    '/ip dhcp-server lease remove [find mac-address=' + mac + ']',
+                ])
+                sendMessage("❌ Device Denied ❌\nHostname: *" + msgencode(host) +
+                    "*\nIP: *" + msgencode(ip) + "*\nMAC Address: *" + mac + "*"
+                )
+                deleteMessage(message_id)
+
+            result[mac] =  {
+                "comment": comment,
+                "ip": ip
+            }
+
+        print(result)
+        logging(result)
+        logout(netmiko, rosapi)
+        return jsonify(result)
+    else:
+        print("Feedback: Related DHCP lease not found.")
+        return jsonify({"status":False,"data":"Related DHCP lease not found."})
+
+
+
 ## begin /webhook
 @app.route('/webhook', methods = ['POST'])
 def webhook():
-    interface = os.environ.get('DHCP_INTERFACE')
-
     if request.is_json:
         input = request.get_json()
         print(json.dumps(input, indent=4))
         message_id = input['callback_query']['message']['message_id']
         message_data = input['callback_query']['data']
-        input = json.loads(message_data)
-
-        netmiko = netmiko_conn(router, username, password)
-        rosapi = rosapi_conn(router, username, password)
-        api = rosapi.get_api()
-
-        # get dhcp lease based on mac-address
-        leases = api.get_resource('ip/dhcp-server/lease')
-        dhcp = leases.get(mac_address=input['mac'], dynamic="yes")
-
-        if dhcp:
-            result = {}
-            for item in dhcp:
-                # get the id only
-                host = item['host-name']
-                ip = item['address']
-                mac = item['mac-address']
-
-                if input['action'] == 'allow':
-                    comment = "allow " + host
-                    netmiko.send_config_set([
-                        '/ip dhcp-server lease make-static [find mac-address=' + mac + ']',
-                        '/ip dhcp-server lease set [find mac-address=' + mac + '] comment=' + comment,
-                        '/ip arp add address=' + ip + ' mac-address=' + mac + ' interface=' + interface
-                    ])
-                    sendMessage("✅ Device Allowed ✅\nHostname: *" + msgencode(host) +
-        "*\nIP: *" + msgencode(ip) + "*\nMAC Address: *" + mac + "*")
-                    deleteMessage(message_id)
-                else:
-                    comment = "deny " + host
-                    netmiko.send_config_set([
-                        '/interface wireless access-list add authentication=no forwarding=no mac-address=' + mac + ' comment="' + comment + '"',
-                        '/ip dhcp-server lease remove [find mac-address=' + mac + ']',
-                    ])
-                    sendMessage("❌ Device Denied ❌\nHostname: *" + msgencode(host) +
-        "*\nIP: *" + msgencode(ip) + "*\nMAC Address: *" + mac + "*")
-                    deleteMessage(message_id)
-
-                result[mac] =  {
-                    "comment": comment,
-                    "ip": ip
-                }
-
-            print(result)
-            logging(result)
-            logout(netmiko, rosapi)
-            return jsonify(result)
-        else:
-            print("Feedback: Related DHCP lease not found.")
-            return jsonify({"status":False,"data":"Related DHCP lease not found."})
-
+        authorization(message_id, message_data)
+        
     else:
         print("Feedback: Wrong JSON format.")
         return jsonify({"status":False,"data":"Wrong JSON format."})
@@ -240,40 +216,32 @@ def webhook():
 @app.route('/push_notif', methods = ['POST'])
 def push_notif():
     url = "https://api.telegram.org/bot" + api_bot + "/sendMessage"
-    print(url)
     if request.form:
         data = request.form.copy()
         now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         host = msgencode(data['host'])
         ip = msgencode(data['ip'])
         mac = data['mac']
-
-        headers = {
-        'Content-Type': 'application/json'
-        }
-        payload = {
-            "chat_id": chat_id,
-            "text": "\\=\\=\\=\\= New Device Connected \\=\\=\\=\\=\nHostname: *" + host +
-            "*\nIP: *" + ip + "*\nMAC Address: *" + mac + "*\nConnected At: " + now + "",
-            "parse_mode": "MarkdownV2",
-            "disable_web_page_preview": True,
-            "reply_markup": {
-                "inline_keyboard": [
-                    [
-                        {
-                            "text": "ALLOW",
-                            "callback_data": "{\"action\":\"allow\",\"mac\":\""+mac+"\"}"
-                        },
-                        {
-                            "text": "DENY",
-                            "callback_data": "{\"action\":\"deny\",\"mac\":\""+mac+"\"}"
-                        }
-                    ]
+        message = "\\=\\=\\=\\= New Device Connected \\=\\=\\=\\=\nHostname: *" + host + "*\nIP: *" + ip + "*\nMAC Address: *" + mac + "*\nConnected At: " + now + ""
+        keyboard = {
+            "inline_keyboard": [
+                [
+                    {
+                        "text": "ALLOW",
+                        "callback_data": "{\"action\":\"allow\",\"mac\":\""+mac+"\"}"
+                    },
+                    {
+                        "text": "DENY",
+                        "callback_data": "{\"action\":\"deny\",\"mac\":\""+mac+"\"}"
+                    }
                 ]
-            }
+            ]
         }
-        response = requests.request("POST", url, headers=headers, data=json.dumps(payload)).json()
-        print(response)
+
+        sendMessage(message, keyboard)
+        #response = sendMessage(message, keyboard)
+        print("=========== Feedback: Notification sent ===========")
+        #print(response)
         return "Feedback: Notification sent"
     else:
         print("Feedback: Empty Data Received")
