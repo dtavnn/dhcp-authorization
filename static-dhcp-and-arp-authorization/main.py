26efdd5f90b359e2f2eca65a7d498a46f3eabe17
xdnroot <admin@tokomini.net>
2021-02-25 14:58:14 +0700

    fix everything

--- static-dhcp-and-arp-authorization/main.py
+++ static-dhcp-and-arp-authorization/main.py
@@ -337,6 +337,7 @@ def showMac(message_data):
         if dhcp:
             for item in dhcp:
                 # get the id only
+                print(json.dumps(item, indent=4))
                 host = item['host-name']
                 ip = item['address']
                 mac = item['mac-address']
